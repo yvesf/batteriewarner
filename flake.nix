d0fa6a32927dfbde4dadf61b0eea49b064855584
Yves Fischer <yvesf+git@xapek.org>
2021-04-15 20:38:32 +0200

    add flake.nix

---
+++ flake.nix
@@ -0,0 +1,10 @@
+{
+  inputs.nixpkgs.url = "nixpkgs/nixos-20.09";
+  outputs = { self, nixpkgs }:
+  with import nixpkgs { system = "x86_64-linux"; };
+  {
+    defaultPackage.x86_64-linux = callPackage ./. {};
+    nixosModule = { config }: { imports = [ ./module.nix ]; };
+  };
+}
+
