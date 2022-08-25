efc84d128aa88a49516abf3e593ea41753f57729
Yves Fischer <yvesf+git@xapek.org>
2022-08-25 22:55:03 +0200

    apply nixpkgs-fmt on .nix files

--- module.nix
+++ module.nix
@@ -1,21 +1,21 @@
 { config, lib, pkgs, ... }:
 let
-  package = pkgs.callPackage ./. {};
+  package = pkgs.callPackage ./. { };
   cfg = config.programs.batteriewarner;
 in
-  {
-    options.programs.batteriewarner = {
-      enable = lib.mkEnableOption "Batteriewarner";
-    };
-    config = lib.mkIf cfg.enable {
-      systemd.services.batteriewarner = {
-        enable = true;
-        description = "Batteriewarner";
-        wantedBy = [ "multi-user.target" ];
-        serviceConfig = {
-          ExecStart = "${package}/bin/batteriewarner";
-        };
+{
+  options.programs.batteriewarner = {
+    enable = lib.mkEnableOption "Batteriewarner";
+  };
+  config = lib.mkIf cfg.enable {
+    systemd.services.batteriewarner = {
+      enable = true;
+      description = "Batteriewarner";
+      wantedBy = [ "multi-user.target" ];
+      serviceConfig = {
+        ExecStart = "${package}/bin/batteriewarner";
       };
     };
-  }
+  };
+}
 
